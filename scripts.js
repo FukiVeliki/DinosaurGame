65ca7bc84a225c93b16dded073ce6c8004b4ada7
FukiVeliki <fuad.ljajic@stu.ssst.edu.ba>
2023-06-06 16:04:14 +0200

    Added basic js elements as well as variables and a funtion for player score

---
+++ scripts.js
@@ -0,0 +1,22 @@
+let container = document.querySelector("#container");
+let dino = document.querySelector("#dino");
+let block = document.querySelector("#block");
+let road = document.querySelector("#road");
+let cloud = document.querySelector("#cloud");
+let score = document.querySelector("#score");
+let gameOver = document.querySelector("#gameOver");
+
+
+// declaring variable for score
+
+let interval = null;
+let playerScore = 0;
+
+//function for score
+
+let scoreCounter = ()=>{
+    playerScore++;
+    score.innerHTML = `Score <b>${playerScore}</b>`;
+}
+
+interval = setInterval(scoreCounter,200);
